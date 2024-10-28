5927a2e53c1d2d239f3fb12f9414edd5661e67af
TV-Trong <tvt180602@gmail.com>
2024-10-28 16:03:39 +0700

    NPC part 2

--- Assets/Scripts/Trọng/TalkToNPC.cs
+++ Assets/Scripts/Trọng/TalkToNPC.cs
@@ -4,16 +4,30 @@ using UnityEngine;
 
 public class TalkToNPC : MonoBehaviour
 {
+    [SerializeField] private GameObject interactButton;
+    private NPC_Behaviour newNPC;
+    private void Start()
+    {
+        interactButton.SetActive(false);
+    }
     private void OnTriggerStay2D(Collider2D collision)
     { 
         if (collision.CompareTag("NPC"))
         {
-            if (Input.GetKeyDown(KeyCode.F))
+            newNPC = collision.GetComponentInParent<NPC_Behaviour>();
+            if (newNPC.isInteractable == true)
             {
-                NPC_Behaviour newNPC;
-                newNPC = collision.GetComponentInParent<NPC_Behaviour>();
-                if (newNPC.isTalking == false) newNPC.Interact();
+                interactButton.SetActive(true);
+                if (Input.GetKeyDown(KeyCode.F)) newNPC.Interact();
             }
         }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("NPC")) interactButton.SetActive(false);
+    }
+    public void TalkWithNPC()
+    {
+        if (newNPC.isInteractable == true) newNPC.Interact();
     }
 }
