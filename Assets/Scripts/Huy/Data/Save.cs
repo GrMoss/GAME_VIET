b905a0b5c950505bb1d5c26b0180248d6244cf62
GrMoss <126696013+GrMoss@users.noreply.github.com>
2024-11-05 02:52:55 +0700

    .

--- Assets/Scripts/Huy/Data/Save.cs
+++ Assets/Scripts/Huy/Data/Save.cs
@@ -6,20 +6,18 @@ using UnityEngine.UI;
 
 public class Save : MonoBehaviour 
 {
-    // public void ButtonSave()
-    // {
-    //     Debug.Log("IsWork1");
-    //     Player.Instance.SavePlayer(); 
-    //     Player.Instance.positionPlayer[0] = transform.position.x;
-    //     Player.Instance.positionPlayer[1] = transform.position.y;
-    //     Player.Instance.positionPlayer[2] = transform.position.z;
-    // }
+    public void ButtonSave()
+    {
+        Player.Instance.SavePlayer(); 
+        Player.Instance.positionPlayer[0] = transform.position.x;
+        Player.Instance.positionPlayer[1] = transform.position.y;
+        Player.Instance.positionPlayer[2] = transform.position.z;
+    }
 
-    //  public void ButtonLoad()
-    // {
-    //     Debug.Log("IsWork2");
-    //     Player.Instance.DisplayAllPlayers();
-    //     Debug.Log(Application.persistentDataPath);
-    // }
+     public void ButtonLoad()
+    {
+        Player.Instance.DisplayAllPlayers();
+        Debug.Log(Application.persistentDataPath);
+    }
 
 }
