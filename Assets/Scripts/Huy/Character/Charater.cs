4ece2afb7b313b03cc2fd234e26b5e3e48af4650
GrMoss <126696013+GrMoss@users.noreply.github.com>
2024-11-08 17:27:16 +0700

    Fix spawn player

---
+++ Assets/Scripts/Huy/Character/Charater.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Charater 
+{
+    public int gender;
+    public Sprite spritePlayer;
+    public RuntimeAnimatorController animatorController;
+}
